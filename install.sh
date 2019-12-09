437dc4d6e7229b9d62052e999b0e44cb89a0164d
tuiboku1128 <tuiboku1128@gmail.com>
2019-12-09 22:53:37 +0900

    delete vscode file

--- install.sh
+++ install.sh
@@ -4,3 +4,9 @@ ln -fs ~/dotfiles/_zshenv ~/.zshenv
 ln -fs ~/dotfiles/_npmrc ~/.npmrc
 # ln -fs ~/dotfiles/_gitconfig ~/.gitconfig
 ln -fs ~/dotfiles/_fzf.zsh ~/.fzf.zsh
+
+cd ~/Library/Application\ Support/Code/User/
+rm settings.json
+ln -s ~/dotfiles/vscode/settings.json ./settings.json
+rm keybindings.json
+ln -s ~/dotfiles/vscode/keybindings.json ./keybindings.json
