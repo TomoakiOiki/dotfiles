7ff30c9acd6216c8746b0de9dba415c191ea3584
tuiboku1128 <tuiboku1128@gmail.com>
2019-12-09 21:27:42 +0900

    a

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
-mv ~/.cache ~/dotfiles/
-
 ln -fs ~/dotfiles/_vimrc ~/.vimrc
 ln -fs ~/dotfiles/_zshrc ~/.zshrc
 ln -fs ~/dotfiles/_zshenv ~/.zshenv
 ln -fs ~/dotfiles/_npmrc ~/.npmrc
-ln -fs ~/dotfiles/_gitconfig ~/.gitconfig
+# ln -fs ~/dotfiles/_gitconfig ~/.gitconfig
 ln -fs ~/dotfiles/_fzf.zsh ~/.fzf.zsh
-
